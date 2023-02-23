f9cbca6bd73eb10fc8e8a0a1b21edef7eb17224f
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2023-02-23 19:06:17 +0300

    initial commit

---
+++ OpenCVStart/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <string>
+#include <fstream>
+
+
+
+int main()
+{
+    std::string path = "Resources/test.png";
+    cv::Mat img = cv::imread(path);
+    cv::imshow("Imagesss", img);
+    cv::waitKey(0);
+
+    return 0;
+
+}
